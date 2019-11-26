e6914bf961b6f5efdfd45ca36e996a448826fc3e
bharath0214 <55925108+bharath0214@users.noreply.github.com>
2019-11-26 12:16:46 +0530

    Update settings.py

--- classification/settings.py
+++ classification/settings.py
@@ -130,3 +130,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
